5f0e1c223f6b6e4f0d2d13ddafe9c4e67732aa10
Quan. Nguyen Dong - CMC Global TTS <ndquan6@cmcglobal.vn>
2025-11-06 11:58:48 +0700

    Fix English Validation

---
+++ src/main/java/com/quanlyduan/project_manager_api/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.quanlyduan.project_manager_api.repository;
+
+import com.quanlyduan.project_manager_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    
+    Optional<User> findByEmail(String email);
+    
+    Boolean existsByEmail(String email);
+}
