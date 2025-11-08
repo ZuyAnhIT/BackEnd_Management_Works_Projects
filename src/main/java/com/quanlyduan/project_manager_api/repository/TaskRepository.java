b20945cc963e5d303b06f25a6a917a074d5a89c0
VietDuc005 <ducsmile111@gmail.com>
2025-11-08 15:45:55 +0700

    feat: repository task

--- src/main/java/com/quanlyduan/project_manager_api/repository/TaskRepository.java
+++ src/main/java/com/quanlyduan/project_manager_api/repository/TaskRepository.java
@@ -1,4 +1,15 @@
 package com.quanlyduan.project_manager_api.repository;
 
-public interface TaskRepository {
+import com.quanlyduan.project_manager_api.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    // Spring Data JPA tự động cung cấp 'findById(Integer taskId)'.
+    // Hàm này là đủ để SecurityService tìm Task và lấy 'projectId' từ nó.
+
+    // 'existsById(Integer taskId)' cũng được cung cấp,
+    // dùng để TaskCommentServiceImpl kiểm tra Task có tồn tại không.
 }
