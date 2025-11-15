99094c41bd2b289fc07302cc62ed20f587000900
Nguyen Viet Duc <ducsmile1111@gamil.com>
2025-11-15 18:27:49 +0700

    feat: file service

--- src/main/java/com/quanlyduan/project_manager_api/service/DashboardService.java
+++ src/main/java/com/quanlyduan/project_manager_api/service/DashboardService.java
@@ -1,4 +1,15 @@
 package com.quanlyduan.project_manager_api.service;
 
+import com.quanlyduan.project_manager_api.dto.response.MyProjectResponse;
+import java.util.List;
+
 public interface DashboardService {
+
+    // (Có thể đã có các phương thức khác ở đây)
+
+    /**
+     * US-S3-3: Lấy danh sách các project mà người dùng hiện tại đang tham gia.
+     * @return Danh sách MyProjectResponse
+     */
+    List<MyProjectResponse> getMyProjects();
 }
