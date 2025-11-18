79180ca9bad0d3b0d0c9256afea609fc253197a7
Anh. Nguyen Ba Duy - CMC Global TTS <nbdanh@cmcglobal.vn>
2025-11-18 10:12:03 +0700

    Thay doi status list member

--- src/main/java/com/quanlyduan/project_manager_api/model/common/enums/CombinedMemberStatus.java
+++ src/main/java/com/quanlyduan/project_manager_api/model/common/enums/CombinedMemberStatus.java
@@ -3,6 +3,7 @@ package com.quanlyduan.project_manager_api.model.common.enums;
 
 public enum CombinedMemberStatus {
     ACTIVE,    // Đã là thành viên và đang "Hoạt động"
-    INACTIVE,  // Đã là thành viên nhưng "Tạm dừng" hoặc "Đã rời"
+   SUSPENDED, // Đã là thành viên nhưng bị "Tạm dừng"
+    REMOVED,   // Đã là thành viên nhưng bị "Xóa/Rời khỏi"
     PENDING    // Chưa là thành viên, lời mời đang "Chờ"
 }
